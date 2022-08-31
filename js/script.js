4e2a4d9fce83ceff377f826ea7da5155b08feb86
deltadasz <58606011+deltadasz@users.noreply.github.com>
2022-08-31 15:57:32 +0300

    new

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Make mobile navigation 
+
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', function() {
+    headerEl.classList.toggle('nav-open');
+});
+
+// Sticky navigation //
+
+const sectionHeroEl = document.querySelector('.section-hero');
+const obs = new IntersectionObserver(function(entries){
+    const ent = entries[0];
+    console.log(ent);
+
+    if(ent.isIntersecting === false){
+        document.body.classList.add('sticky');
+    }
+    if(ent.isIntersecting === true){
+        document.body.classList.remove('sticky');
+    }
+},
+{
+    root: null,
+    threshold: 0,
+    rootMargin:'76px'
+}
+);
+
+obs.observe(sectionHeroEl);
+
